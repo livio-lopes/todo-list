2967face324d666c42a56e4910f72f6824ca4c61
Lívio Lopes <antonio.lvio760@gmail.com>
2023-11-03 18:52:01 -0300

    feat(app.tsx): create thre routes for Login, TodoList and AddTodo pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Login from '../src/pages/Login'
 import TodoList from './pages/TodoList'
 import AddTodo from './pages/AddTodo'
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
 
 
   return (
     <>
-        <Login />
-        <TodoList />
-        <AddTodo /> 
+        <Routes >
+            <Route path='/todo/add' element={<AddTodo />}/>
+            <Route path='/todo/' element={<TodoList />}/> 
+            <Route path='/' element={<Login />}/> 
+        </Routes>
     </>
   )
 }
